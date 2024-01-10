6d49e0fb7653e8414e96b46cb984eadbf6aeea3a
dajunomo <100373195+dajunomo@users.noreply.github.com>
2024-01-10 00:12:45 -0500

    Update Reto.php

--- Reto.php
+++ Reto.php
@@ -1,5 +1,30 @@
 <?php
 
+/* $uno = 1;
+$dos = 1;
+$tres = 2;
+$cuatro = 3;
+$cinco = 5;
+$seis= 8;
+$siete = 13;
+$ocho = 21; */
+
+$numero_tienda = 3;
+$anterior = 0;
+$actual= 1;
+
+
+for ($i=2; $i <= $numero_tienda ; $i++) { 
+    
+    $temporal = $actual;
+    $actual += $anterior;
+    $anterior = $temporal;
+
+    echo $actual . "\n";
+    echo "\n";
+
+};
+
 
 
 
