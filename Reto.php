ba08b743e4505477979bdbf3c0996f875d77b9d8
dajunomo <100373195+dajunomo@users.noreply.github.com>
2024-01-10 00:22:08 -0500

    Update Reto.php

--- Reto.php
+++ Reto.php
@@ -9,7 +9,7 @@ $seis= 8;
 $siete = 13;
 $ocho = 21; */
 
-$numero_tienda = 3;
+$numero_tienda = (int) readline ("dime el numero de tienda: ");
 $anterior = 0;
 $actual= 1;
 
@@ -20,10 +20,11 @@ for ($i=2; $i <= $numero_tienda ; $i++) {
     $actual += $anterior;
     $anterior = $temporal;
 
-    echo $actual . "\n";
-    echo "\n";
+   
 
 };
+
+echo "hay  $actual formas de llegar"
 
 
 
